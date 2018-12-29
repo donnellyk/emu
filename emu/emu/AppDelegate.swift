6e837e18de1f4f4a42e45de46e9dc0163606d76a
Kevin <kevin@kvnd.me>
2018-12-29 17:31:46 -0600

    Switch to Cocoa-based project
    Because its a pain in the ass to get a window in a non-Cocoa project, and this makes testing easier

---
+++ emu/emu/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  emu
+//
+//  Created by Kevin Donnelly on 12/29/18.
+//  Copyright © 2018 Kevin Donnelly. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
