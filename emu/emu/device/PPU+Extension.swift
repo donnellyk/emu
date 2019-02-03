61bdc9bfc2c8dd369d37116f2ffb758bc46f2c50
Kevin <kevin@kvnd.me>
2019-02-03 17:20:44 -0600

    Palette register helper

--- emu/emu/device/PPU+Extension.swift
+++ emu/emu/device/PPU+Extension.swift
@@ -1,5 +1,21 @@
 import Foundation
 
+// MARK: COMPUTED VARS
+public extension PPU {
+  var bgPalette: Palette {
+    return Palette(value: mmu.read(.bgPalette))
+  }
+  
+  var objPalette0: Palette {
+    return Palette(value: mmu.read(.objPalette0))
+  }
+  
+  var objPalette1: Palette {
+    return Palette(value: mmu.read(.objPalette1))
+  }
+}
+
+// MARK: DEFINITIONS
 public extension PPU {
   public enum Mode : UInt8 {
     case oamSearch = 2
@@ -65,6 +81,75 @@ public extension PPU {
       x = memory[1]
       chrCode = memory[2]
       attributes = memory[3]
+    }
+  }
+  
+  public struct Palette {
+    enum Color : UInt8 {
+      case white, lightGrey, darkGrey, black
+      
+      var value: (r: UInt8, g: UInt8, b: UInt8) {
+        switch self {
+        case .white:
+          return (0xFF, 0xFF, 0xFF)
+        case .lightGrey:
+          return (0xCC, 0xCC, 0xCC)
+        case .darkGrey:
+          return (0x77, 0x77, 0x77)
+        case .black:
+          return (0x00, 0x00, 0x00)
+        }
+      }
+    }
+    
+    let value: UInt8
+    
+    func getColor(num: UInt8) -> Color {
+      switch num {
+      case 0b00:
+        return Color(rawValue: value.get(bits: 1, 0))!
+      case 0b01:
+        return Color(rawValue: value.get(bits: 3, 2))!
+      case 0b10:
+        return Color(rawValue: value.get(bits: 5, 4))!
+      default:
+        return Color(rawValue: value.get(bits: 7, 6))!
+      }
+    }
+  }
+
+  
+  public struct BitMap {
+    private(set) var data = Data()
+    
+    public struct Line {
+      private(set) var data = Data()
+      
+      mutating func addPixel(_ pixel: Pixel) {
+        data.append(contentsOf: pixel.data)
+      }
+      
+      mutating func addPixels(_ pixels: [Pixel]) {
+        pixels.forEach{ addPixel($0) }
+      }
+    }
+    
+    public struct Pixel {
+      let data: Data
+      
+      init(color: Palette.Color) {
+        let value = color.value
+        data = Data()
+      }
+    }
+    
+    
+    mutating func addLine(_ line: Line) {
+      data.append(contentsOf: line.data)
+    }
+    
+    func makeImage() -> CGImage? {
+      return nil
     }
   }
 }
