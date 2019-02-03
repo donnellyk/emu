edfd41dc78c1343639149b6cd9c2f4b1d53d0cb5
Kevin <kevin@kvnd.me>
2019-02-03 17:21:07 -0600

    Draw line of Pixels

--- emu/emu/device/PPU.swift
+++ emu/emu/device/PPU.swift
@@ -28,6 +28,8 @@ public class PPU {
     }
   }
   
+  var bitmap = BitMap()
+  
   init(mmu: MMU) {
     self.mmu = mmu
   }
@@ -91,10 +93,13 @@ public extension PPU {
       return
     }
     
+    var line = BitMap.Line()
+    
     // Render Background
     if mmu.lcdc.bgDisplayFlag {
       for i in 0..<20 {
-        drawLine(fetchTileLine(index: i))
+        let pixels = renderTile(bytes: fetchTileLine(index: i))
+        line.addPixels(pixels)
       }
     }
     
@@ -107,39 +112,61 @@ public extension PPU {
     if mmu.lcdc.objFlag {
       
     }
+    
+    bitmap.addLine(line)
   }
   
-  func fetchTileLine(index: Int) -> UInt16 {
+  func fetchTileLine(index: Int) -> (UInt8, UInt8) {
     let bgChar = mmu.lcdc.bgCharArea
     
     // ((line + syc) % 8) = current "line" within a tile. * 2 to get address offset, since each pixel line is 2 bytes
     let tileLineOffset = UInt16(line &+ mmu.read(.scy)) % 8 * 2
     let id = UInt16(fetchTileID(index: UInt16(index)))
     
-    let tileAddress = bgChar + (id * PPU.tileSize) + tileLineOffset
+    let tileAddress = bgChar + (id * PPU.tileSize) // Address of beginning of the tile
+    let lineAddress = tileAddress + tileLineOffset // Address for specific line of 8 pixels from tile
     
-    return mmu.read(tileAddress)
+    return (mmu.read(lineAddress), mmu.read(lineAddress+1))
   }
-
 
   /// Reads the tile id for the given line and index
   ///
   /// - Parameter index: index of tile in the current line. Ex: First tile is 0, last is 19
   /// - Returns: The ID of the tile, to be read from `BGCHAR`
-  func fetchTileID(index: UInt16) -> UInt8 {
+  /// - Discussion: The ID will be adjusted as necessary based on the BGCodeArea flag, with the necessary offsets applied to be a positive value. Use as is.
+  func fetchTileID(index: UInt16) -> UInt16 {
     let x = UInt16(mmu.read(.scx)) / PPU.tileIDSize
     let y = UInt16(line &+ mmu.read(.scy)) / PPU.tileIDSize
     
     let coord = (y * PPU.bgWidth) + x
     let idAddress = mmu.lcdc.bgCodeArea + coord + UInt16(index)
-    return mmu.read(idAddress) &+ mmu.lcdc.bgTileIDOffset
+    let bitPattern:UInt8 = mmu.read(idAddress)
+    
+    
+    if mmu.lcdc.bgCharAreaFlag {
+      let offset: Int16 = 128
+      let signedValue = Int16(clamping: Int8(bitPattern: bitPattern))
+      
+      return UInt16(clamping: signedValue + offset)
+    } else {
+      return UInt16(clamping: bitPattern)
+    }
   }
   
-  func drawLine(_ line: UInt16) {
+  func renderTile(bytes: (UInt8, UInt8)) -> [BitMap.Pixel] {
+    return (0..<8).map { renderPixel(i: $0, bytes: bytes) }
+  }
+  
+  func renderPixel(i index: Int, bytes: (UInt8, UInt8)) -> BitMap.Pixel {
+    var bit = bytes.0.bit(index) << 1
+    bit |= bytes.1.bit(index)
     
+    let color = bgPalette.getColor(num: bit)
+    return BitMap.Pixel(color: color)
   }
   
   func pushToScreen() {
     // Might not be necessary, depending on the screen implementation
+    bitmap = BitMap() //Reset bitmap
   }
 }
