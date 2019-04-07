72cc4940cb8da2ab8cdab5307bff1e3ecd678590
Kevin <kevin@kvnd.me>
2019-04-06 20:38:42 -0500

    Add debug service and controller
    Breakpoints, register printed, instruction history

---
+++ emu/emu/Debug/DebugService.swift
@@ -0,0 +1,119 @@
+import Cocoa
+
+extension NSNotification.Name {
+  static let DebugServiceDidPause = NSNotification.Name("DebugServiceDidPause")
+  static let DebugServiceProgramDidUpdate = NSNotification.Name("DebugServiceProgramDidUpdate")
+}
+class DebugService {
+  static var shared: DebugService = DebugService()
+  private init() { }
+  
+  private(set) var pcBreakpoint: UInt16?
+  private(set) var isPaused: Bool = false
+  
+  private var executionHistory = [String]() {
+    didSet {
+      if executionHistory.count > 100 {
+        executionHistory.removeFirst()
+      }
+    }
+  }
+  
+  private var device: Device! {
+    return (NSApplication.shared.delegate as! AppDelegate).device
+  }
+}
+
+// MARK: - Device Interface
+extension DebugService {
+  func shouldStep() -> Bool {
+    if !isPaused {
+      checkForBreakpoint()
+    }
+    
+    if isPaused {
+      print("---------- PAUSED ----------")
+    }
+    
+    return !isPaused
+  }
+  
+  func performingInstruction(opSummary: String, programCounter: UInt16) {
+    print("Instruction Executed: \(opSummary); \(programCounter.toHex)")
+    
+    executionHistory.append(opSummary)
+  }
+  
+  func didStep() {
+    NotificationCenter.default.post(name: .DebugServiceProgramDidUpdate, object: nil)
+  }
+}
+
+private extension DebugService {
+  func checkForBreakpoint() {
+    if shouldBreak() {
+      isPaused = true
+      NotificationCenter.default.post(name: .DebugServiceDidPause, object: nil)
+    }
+  }
+  
+  func shouldBreak() -> Bool {
+    return device.cpu.registers.pc == pcBreakpoint
+  }
+}
+
+// MARK: - UI Interface
+extension DebugService {
+  var displayBreakpoint: String {
+    return pcBreakpoint?.toHex ?? ""
+  }
+  
+  
+  /// Returns register value in a sane/readable format
+  var prettyRegisterValues: String {
+    return """
+    \(device.cpu.registers.prettyPrint())
+    """
+  }
+  
+  /// Returns flags value in a sane/readable format
+  var prettyHardwareRegisterValues: String {
+    return """
+    Hardware Register
+    \(device.cpu.mmu.lcdc.prettyPrint())
+    \(device.cpu.mmu.stat.prettyPrint())
+    """
+  }
+  
+  func step() {
+    device.debugStep()
+  }
+  
+  func pause() {
+    isPaused = true
+  }
+  
+  func resume() {
+    isPaused = false
+    device.debugResume()
+  }
+  
+  func clearBreakpoint() {
+    pcBreakpoint = nil
+  }
+  
+  func setBreakpoint(_ adr: String) -> Bool {
+    let formatedString = adr.replacingOccurrences(of: "0x", with: "")
+    guard let intValue = UInt16(formatedString, radix: 16) else {
+      pcBreakpoint = nil
+      return false
+    }
+    
+    pcBreakpoint = intValue
+    return true
+  }
+  
+  func generateProgram() -> String {
+    return executionHistory.joined(separator: "\n")
+  }
+}
---
+++ emu/emu/Debug/DebugViewController.swift
@@ -0,0 +1,80 @@
+import Cocoa
+
+class DebugViewController: NSViewController {
+  @IBOutlet weak var pauseButton: NSButton!
+  @IBOutlet weak var stepButton: NSButton!
+  @IBOutlet weak var addressField: NSTextField!
+  @IBOutlet weak var addressLabel: NSTextField!
+  @IBOutlet var programField: NSTextView!
+  @IBOutlet weak var registerLabel: NSTextField!
+  @IBOutlet weak var flagLabels: NSTextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(resetButtons),
+      name: .DebugServiceDidPause,
+      object: nil
+    )
+    
+//    NotificationCenter.default.addObserver(
+//      self,
+//      selector: #selector(updateProgramDisplay),
+//      name: .DebugServiceProgramDidUpdate,
+//      object: nil
+//    )
+    
+    resetButtons()
+    updateProgramDisplay()
+    
+    addressLabel.stringValue = DebugService.shared.displayBreakpoint
+  }
+  
+  // MARK: - ACTIONS
+  @IBAction func setTapped(_ sender: Any) {
+    defer {
+      addressField.stringValue = ""
+    }
+    _ = DebugService.shared.setBreakpoint(addressField.stringValue)
+    addressLabel.stringValue = DebugService.shared.displayBreakpoint
+  }
+  
+  @IBAction func stepTapped(_ sender: Any) {
+    DebugService.shared.step()
+    updateProgramDisplay()
+  }
+  
+  @IBAction func pauseTapped(_ sender: Any) {
+    defer {
+      resetButtons()
+      updateProgramDisplay()
+    }
+    
+    if DebugService.shared.isPaused {
+      DebugService.shared.resume()
+    } else {
+      DebugService.shared.pause()
+    }
+  }
+  
+  @objc func resetButtons() {
+    if DebugService.shared.isPaused {
+      pauseButton.title = "Resume"
+      stepButton.isHidden = false
+    } else {
+      pauseButton.title = "Pause"
+      stepButton.isHidden = true
+    }
+  }
+  
+  @objc func updateProgramDisplay() {
+    programField.string = DebugService.shared.generateProgram()
+    
+    registerLabel.stringValue = DebugService.shared.prettyRegisterValues
+    flagLabels.stringValue = DebugService.shared.prettyHardwareRegisterValues
+  }
+  
+  
+}
