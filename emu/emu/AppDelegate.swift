0144c9662734d6f46966394b91603ec1183e0caf
Kevin <kevin@kvnd.me>
2020-04-05 09:53:31 -0500

    Show debug window on launch

--- emu/emu/AppDelegate.swift
+++ emu/emu/AppDelegate.swift
@@ -2,15 +2,21 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+  lazy private var mainStoryboard = NSStoryboard(name: "Main", bundle: nil)
+  
   var device: Device! = nil
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    DebugService.shared.setBreakpoint("0x005b")
+    _ = DebugService.shared.setBreakpoint("0x005b")
     
     device = Device()
     device.screen = (NSApplication.shared.mainWindow?.windowController?.contentViewController as? ViewController)
-
+    
     device.boot(cartridge: Cartridge())
+    
+    let debugController: NSWindowController = mainStoryboard.instantiateController(identifier: "DebugWindowController")
+    
+    debugController.showWindow(self)
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
