a77722d1a6798c95927917f2ba662e838050ba4e
Kevin <kevin@kvnd.me>
2020-04-13 21:43:44 -0500

    Helper function for color tests

--- emu/emu/Canvas.swift
+++ emu/emu/Canvas.swift
@@ -545,6 +545,19 @@ protocol ConvertibleToNSColor {
   var color : NSColor { get }
 }
 
+extension ConvertibleToNSColor {
+  var hexValue: String {
+    guard let rgbColor = color.usingColorSpace(.deviceRGB) else {
+      return "#FFFFFF"
+    }
+    let red = Int(round(rgbColor.redComponent * 0xFF))
+    let green = Int(round(rgbColor.greenComponent * 0xFF))
+    let blue = Int(round(rgbColor.blueComponent * 0xFF))
+    let hexString = NSString(format: "#%02X%02X%02X", red, green, blue)
+    return hexString as String
+  }
+}
+
 extension NSColor : ConvertibleToNSColor {
   var color : NSColor {
     return self
